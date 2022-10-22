5ec1a5c94f75a43b3a6f1106416fe9dbaf3f4088
Fazlu601 <fazrlu9575@gmail.com>
2022-10-22 21:58:15 +0700

    add: integrasi api ke article dan informasi di homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
-//Main script
+import { getApi } from "./data.js";
+import { renderInformation, renderArticle } from "./dom.js";
+import { API, newsAPI } from "./env.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+    getApi(newsAPI).then( data => {
+        renderInformation(data);
+
+    });
+
+    getApi(API).then( data => {
+        
+        const contentArticle = document.getElementById('content-article');
+        contentArticle.innerHTML = "";
+
+        let index = 0;
+        for(let article of data) {
+            if( index < 5 ){
+                let articleElement = renderArticle(article);
+
+                contentArticle.append(articleElement)
+            }
+            index++;
+        }
+    });
+
+});
